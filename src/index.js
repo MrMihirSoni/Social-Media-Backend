9735555b07d83ac14852d45df8040957afb62420
Mihir Soni <ms6711534@gmail.com>
2024-02-10 22:54:54 +0530

    updated cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["https://kind-jade-chicken-tutu.cyclic.app", "http://localhost:5173"],
+  credentials:true
+}));
 app.use(cookieParser());
 app.use(device.capture());
 app.use("/users", userRouter);
